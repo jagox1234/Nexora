99ac4a70e9d196037be94e9135f61452a30115db
jagox1234 <jagox1234@gmail.com>
2025-08-14 23:21:26 +0200

    Commit manual edits in App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler";
 import App from "./Nexora/1_app_main.js";
 // Prueba de sincronización StackBlitz
-// Segunda prueba de sincronización StackBlitz
+// Segunda prueba de sincronización StackBlitz aaaaaaaaaaaa
+// Tercera prueba de sincronización StackBlitz
 export default App;
