0673dde0daff656617cf294f254896b652e46a68
jagox1234 <jagox1234@gmail.com>
2025-08-14 23:14:30 +0200

    Migración inicial Nexora webapp: estructura base y componentes principales

---
+++ webapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Nexora WebApp</h1>
+      <p>Bienvenido a la versión web de Nexora.</p>
+    </div>
+  );
+}
+
+export default App;
